b60c3a6426c87e73808b0f02cb920203ee83d024
jha-amit <thegoodamit@gmail.com>
2022-08-14 15:15:40 -0600

    some tri-connectivity modification

--- cython_Dijkstra.pyx
+++ cython_Dijkstra.pyx
@@ -59,7 +59,7 @@ def node_val(my_type[:, ::1] Hash, my_type[:, ::1] Cost_horizontal, my_type[:, :
     #cdef double[:,::1] node_val2 = Node_val2
    
     cdef double  ip, jp, i1, i2, j, l1, l2
-    cdef double Cijk = 0
+    cdef double Cijk = 0, Cijk1 = 0
     cdef int v1, v2,j1,j2,j3,source_neighbour,sink_neighbour,i,layer
 
     source_neighbour = truncated_layer
@@ -117,39 +117,59 @@ def node_val(my_type[:, ::1] Hash, my_type[:, ::1] Cost_horizontal, my_type[:, :
                         (abs(Hash[v2,1]-Hash[v1,1]) <= 1) and\
                         (abs(Hash[v2,2]-Hash[v1,2]) <=1)):
                         #in this case, add the edge
-                        #determine its (cumulative) cost first
+                        #determine its (cumulative) cost first                        
                         i1 = (Hash[v2,1])
                         i2 = (Hash[v2,2])
+
                         ip = 0.5*(j-i1)
                         jp = 0.5*(j+i1)
-                    
+
                         if abs(Hash[v2,0]-Hash[v1,0])<=1:
+
                             if Hash[v1,1]<=Hash[v2,1]: 
                                 Cijk = Cost_horizontal[int(ip),int(jp)]
                             else:
                                 Cijk = Cost_vertical[int(ip),int(jp)]
-                                
+
                         elif abs(Hash[v2,0]-Hash[v1,0])<=2:
+
                             Cijk = Cost_diag[int(ip),int(jp)]
-        
+
+                            if (Cijk >= Cost_horizontal[int(ip),int(jp)] + Cost_vertical[int(ip),int(jp) - 1])\
+                                        and (Cijk >= Cost_vertical[int(ip),int(jp)] + Cost_horizontal[int(ip)-1,int(jp)]):
+
+                                Cijk = np.inf
+                                #max((Cost_horizontal[int(ip),int(jp)] + Cost_vertical[int(ip),int(jp) - 1]),\
+                                        #(Cost_vertical[int(ip),int(jp)] + Cost_horizontal[int(ip)-1,int(jp)]))
+
+
                         ip = 0.5*(j-i2)
                         jp = 0.5*(j+i2)
-        
-                        if abs(Hash[v2,0]-Hash[v1,0])<=1:
+
+                        if abs(Hash[v2,0]-Hash[v1,0])<=1:                                             
+
                             if Hash[v1,2]<=Hash[v2,2]: 
                                 Cijk = Cost_horizontal[int(ip),int(jp)]+Cijk # changing the inquality here
                             else:
                                 Cijk = Cost_vertical[int(ip),int(jp)]+Cijk
+
                         elif abs(Hash[v2,0]-Hash[v1,0])<=2:
-                            Cijk = Cost_diag[int(ip),int(jp)]+Cijk
-                          
+
+                            Cijk1 = Cost_diag[int(ip),int(jp)]
+
+                            if (Cijk1 >= Cost_horizontal[int(ip),int(jp)] + Cost_vertical[int(ip),int(jp) - 1])\
+                                        and (Cijk1 >= Cost_vertical[int(ip),int(jp)] + Cost_horizontal[int(ip)-1,int(jp)]):
+
+                                Cijk1 = np.inf
+
+                            Cijk = Cijk1+Cijk
+
+
                         if (node_val1[v2,0]>(Cijk+node_val1[v1,0])):
-                            #print(Cijk,node_val1[v1,0])
-                            node_val1[v2,0]=Cijk+node_val1[v1,0]
-                            #print(Cijk,v2)
+                    
+                            node_val1[v2,0]=Cijk+node_val1[v1,0]                            
                             node_val1[v2,1]=v1
                             node_val1[v2,2]=v2
-            #print(node_val1)
 
         else:
             i=0
